90d75e88f795bcf74d8858c80b3c8f2b6c06b8ca
Sunghan <park.libra1011@gmail.com>
2025-05-06 22:16:08 +0900

    init : 프로젝트 초기 설정 추가 * 환경 - JDK 21, SpringBoot 3.4.4, Lombock 추가 * 전체 프로젝트내에서 디렉토리 모듈형식으로 프로젝트 분할 * prography-bff - FE의 VIEW 구성을 위한 백엔드 서비스(8080) * prography-crawler - 음식점 리뷰 및 정보를 수집을 위한 크롤러 서비스(8081) * prography-search - 자연어를 통한 검색 엔진 서비스(8082)

---
+++ prography-crawler/src/main/java/org/prography/PrographyCrawlerApplication.java
@@ -0,0 +1,11 @@
+package org.prography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrographyCrawlerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrographyCrawlerApplication.class, args);
+    }
+}
---
+++ prography-search/src/main/java/org/prography/PrographySearchApplication.java
@@ -0,0 +1,11 @@
+package org.prography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrographySearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrographySearchApplication.class, args);
+    }
+}
