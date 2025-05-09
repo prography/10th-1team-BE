8e35342ab626d9dacbd58726458ddfaeea447303
seogwoojin <zaza0804@uos.ac.kr>
2025-05-09 11:53:19 +0900

    feat: java -> kotlin 마이그레이션

---
+++ prography-crawler/src/test/kotlin/org/prography/KotlinControllerTest.kt
@@ -0,0 +1,11 @@
+package org.prography
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinControllerTest {
+    fun hello() {
+        println()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "10th-1team-BE"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+include(
+    "prography-crawler",
+    "prography-search",
+    "prography-bff"
+)
