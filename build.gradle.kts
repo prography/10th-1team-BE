d86eccad34d39f36c559d8721251d1d1b44f3dbc
seogwoojin <server@uos.ac.kr>
2025-05-22 14:01:27 +0900

    feat: JPA 의존성 추가
    - 우선은 Postgresql 적용했습니다

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
     kotlin("kapt") version "1.9.25"
     id("org.springframework.boot") version "3.4.4" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
