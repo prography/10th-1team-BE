fab139e19e233c9a00bd1754fe82d22c039c30b5
Sunghan <park.libra1011@gmail.com>
2025-05-24 03:31:43 +0900

    feat : 응답 반환값 추가

---
+++ prography-search/src/main/kotlin/org/prography/config/response/ApiResponse.kt
@@ -0,0 +1,17 @@
+package org.prography.config.response
+
+import java.time.LocalDateTime
+
+sealed class ApiResponse<out T> {
+    data class Success<T>(val data: T, val time: LocalDateTime = LocalDateTime.now()) :
+        ApiResponse<T>()
+
+    data class Failure(val error: String, val time: LocalDateTime = LocalDateTime.now()) :
+        ApiResponse<Nothing>()
+
+    companion object {
+        fun <T> success(data: T): ApiResponse<T> = Success(data)
+
+        fun fail(message: String): ApiResponse<Nothing> = Failure(message)
+    }
+}
