bb91cfebb578ea439228a21e14c2e35f6dee3e4a
Sunghan <park.libra1011@gmail.com>
2025-05-24 03:15:47 +0900

    refactor : 정확히 어떤 Elasticsearch 에 연결을 하는지 볼 수 있도록 로그 추가

--- prography-search/src/main/kotlin/org/prography/config/elasticsearch/ElasticsearchConfig.kt
+++ prography-search/src/main/kotlin/org/prography/config/elasticsearch/ElasticsearchConfig.kt
@@ -4,8 +4,11 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient
 import co.elastic.clients.json.jackson.JacksonJsonpMapper
 import co.elastic.clients.transport.ElasticsearchTransport
 import co.elastic.clients.transport.rest_client.RestClientTransport
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import org.apache.http.HttpHost
 import org.elasticsearch.client.RestClient
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -18,12 +21,15 @@ import org.springframework.context.annotation.Configuration
 class ElasticsearchConfig(
     private val props: ElasticsearchProperty,
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     /**
      * Low-level REST Client
      */
     @Bean
     fun retClient(): RestClient {
         val schema = if (props.tls) "https" else "http"
+        log.info("🗄️  Elasticsearch RestClient will connect to {}://{}:{}", schema, props.host, props.port)
         return RestClient.builder(
             HttpHost(props.host, props.port, schema),
         ).build()
@@ -33,15 +39,23 @@ class ElasticsearchConfig(
      * Transport layer for Java client
      */
     @Bean
-    fun transport(restClient: RestClient): ElasticsearchTransport =
-        RestClientTransport(
+    fun transport(restClient: RestClient): ElasticsearchTransport {
+        log.info("🔗  Wrapping RestClient in RestClientTransport with JacksonJsonpMapper")
+        val objectMapper =
+            ObjectMapper()
+                .registerModules(kotlinModule())
+        return RestClientTransport(
             restClient,
-            JacksonJsonpMapper(),
+            JacksonJsonpMapper(objectMapper),
         )
+    }
 
     /**
      * High-level Java client
      */
     @Bean
-    fun elasticsearchClient(transport: ElasticsearchTransport): ElasticsearchClient = ElasticsearchClient(transport)
+    fun elasticsearchClient(transport: ElasticsearchTransport): ElasticsearchClient {
+        log.info("🚀  ElasticsearchClient initialized")
+        return ElasticsearchClient(transport)
+    }
 }
