3514bc42aec00dbb7bdd112e41bc42d9f3d60474
Sunghan <park.libra1011@gmail.com>
2025-05-24 03:31:15 +0900

    refactor : OpLogTs 관련 주석 추가

--- prography-search/src/main/kotlin/org/prography/domain/OplogTs.kt
+++ prography-search/src/main/kotlin/org/prography/domain/OplogTs.kt
@@ -6,8 +6,14 @@ import com.fasterxml.jackson.annotation.JsonProperty
  * oplog_ts 의 T, I 필드를 담는 클래스
  */
 data class OplogTs(
+    /**
+     * 초 단위 Unix 타임스탬프
+     */
     @JsonProperty("T")
     val T: Long = 0,
+    /**
+     * 동일 초 내에서의 카운터
+     */
     @JsonProperty("I")
     val I: Long = 0,
 )
