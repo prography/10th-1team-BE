997a62e20bb0eb6780a53389938b45744321881f
Sunghan <park.libra1011@gmail.com>
2025-05-25 04:01:35 +0900

    feat : Swagger 추가 & 독립적인 애플리케이션으로 GCP에 올릴 수 있도록 수정

--- prography-search/build.gradle.kts
+++ prography-search/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 }
 
 dependencies {
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+
     implementation("org.springframework.data:spring-data-elasticsearch:5.5.0")
     implementation("co.elastic.clients:elasticsearch-java:8.18.1")
 }
