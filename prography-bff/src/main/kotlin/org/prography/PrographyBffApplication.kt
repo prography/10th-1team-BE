6e7487f8917bbcdd804fc25b9a469a0e553c2293
Sunghan <park.libra1011@gmail.com>
2025-06-02 07:57:47 +0900

    refactor : Main 클래스 package 추가 application.yml 엘라스틱 서치 설정 추가

--- prography-bff/src/main/kotlin/org/prography/PrographyBffApplication.kt
+++ prography-bff/src/main/kotlin/org/prography/PrographyBffApplication.kt
@@ -1,9 +1,11 @@
 package org.prography
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = ["org.prography.bff", "org.prography.search"])
+@SpringBootApplication(scanBasePackages = ["org.prography.bff", "org.prography.search"])
 class PrographyBffApplication
 
 fun main(args: Array<String>) {
