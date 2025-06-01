d6952cdd4dd1bfd102b229b019096da8fa8518ff
Sunghan <park.libra1011@gmail.com>
2025-06-01 23:35:39 +0900

    refactor : Elasticsearch 관련 컨트롤러 삭제 후 BFF 통합 * 검색관리 Mock API 제거

--- prography-bff/src/main/kotlin/org/prography/search/controller/SearchController.kt
+++ prography-bff/src/main/kotlin/org/prography/search/controller/SearchController.kt
@@ -6,14 +6,16 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import org.prography.config.response.ApiResponse
 import org.prography.config.response.CursorResponse
+import org.prography.search.controller.model.AutoCompleteResponseDTO
+import org.prography.search.controller.model.SearchResponseDTO
+import org.prography.search.controller.model.enumeration.FoodCategory
+import org.prography.search.controller.model.enumeration.OrderStrategy
 import org.prography.search.model.PlaceDetailDTO
-import org.prography.search.model.PlaceSummaryDTO
-import org.prography.search.model.enumeration.SortType
 
 interface SearchController {
     @Operation(
-        summary = "검색 결과 API",
-        description = "요청의 기준에 따라 적합한 음식점 정보 리스트를 리스트업합니다.",
+        summary = "자동 완성 API",
+        description = "상호명 앞자리가 동일한 부분을 검색",
         responses = [
             io.swagger.v3.oas.annotations.responses.ApiResponse(
                 responseCode = "200",
@@ -22,7 +24,7 @@ interface SearchController {
                         mediaType = "application/json",
                         array =
                             ArraySchema(
-                                schema = Schema(implementation = PlaceSummaryDTO::class),
+                                schema = Schema(implementation = AutoCompleteResponseDTO::class),
                                 arraySchema = Schema(description = "Cursor-paginated list"),
                             ),
                     ),
@@ -30,13 +32,39 @@ interface SearchController {
             ),
         ],
     )
-    fun getMockSummaryList(
-        keyword: String?,
-        lastId: String?,
+    fun autoComplete(
+        keyword: String,
         size: Int,
-        guCodes: List<String>?,
-        sortType: SortType,
-    ): ApiResponse<CursorResponse<PlaceSummaryDTO>>
+        addressCodes: List<String>?,
+    ): ApiResponse<List<AutoCompleteResponseDTO>>
+
+    @Operation(
+        summary = "검색 중 & 검색 완료 API",
+        description = "기본적인 Elasticsearch 매치 쿼리 기본정렬로 반환 됩니다. 마지막 아이디는 리스트뷰의 마지막 원소의 ID 값을 넣으면 조회 됩니다. (hasNext === false 일 경우 다음 스크롤에 불러들일 리스트가 없습니다.)",
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "200",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        array =
+                            ArraySchema(
+                                schema = Schema(implementation = SearchResponseDTO::class),
+                                arraySchema = Schema(description = "Cursor-paginated list"),
+                            ),
+                    ),
+                ],
+            ),
+        ],
+    )
+    fun searchTerm(
+        keyword: String,
+        size: Int,
+        lastId: String?,
+        addressCodes: List<String>?,
+        categories: List<FoodCategory>?,
+        sort: OrderStrategy?,
+    ): ApiResponse<CursorResponse<SearchResponseDTO>>
 
     @Operation(
         summary = "음식점 세부 페이지 정보",
--- prography-bff/src/main/kotlin/org/prography/search/controller/SearchControllerImpl.kt
+++ prography-bff/src/main/kotlin/org/prography/search/controller/SearchControllerImpl.kt
@@ -2,48 +2,86 @@ package org.prography.search.controller
 
 import org.prography.config.response.ApiResponse
 import org.prography.config.response.CursorResponse
+import org.prography.search.controller.model.AutoCompleteResponseDTO
+import org.prography.search.controller.model.Region
+import org.prography.search.controller.model.ReviewSummary
+import org.prography.search.controller.model.SearchResponseDTO
+import org.prography.search.controller.model.enumeration.FoodCategory
+import org.prography.search.controller.model.enumeration.OrderStrategy
 import org.prography.search.model.PlaceDetailDTO
-import org.prography.search.model.PlaceSummaryDTO
-import org.prography.search.model.enumeration.SortType
 import org.prography.search.model.strength.StrengthScoresDto
+import org.prography.search.service.SearchService
 import org.prography.search.service.mock.MockService
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/search")
 class SearchControllerImpl(
     private val mockService: MockService,
+    private val searchService: SearchService,
 ) : SearchController {
-    @GetMapping("/mock")
-    override fun getMockSummaryList(
-        @RequestParam(required = false) keyword: String?,
-        @RequestParam(required = false) lastId: String?,
-        @RequestParam(required = false, defaultValue = "10") size: Int,
-        @RequestParam(required = false) guCodes: List<String>?,
-        @RequestParam(required = false, defaultValue = "GENERAL") sortType: SortType,
-    ): ApiResponse<CursorResponse<PlaceSummaryDTO>> {
-        val codes = guCodes ?: emptyList()
-        val summaries =
-            mockService.getSummaries(keyword, lastId, size)
-                .stream()
-                .map {
-                    PlaceSummaryDTO(
-                        it.id,
-                        it.name,
-                        it.addressName,
-                        it.roadAddressName,
-                        it.kakaoReviews,
-                        it.kakaoScore,
-                        it.naverReviews,
-                        it.naverScore,
-                    )
-                }.toList()
-        val hasNext = mockService.isLatest(summaries.last().id)
+    @GetMapping("/auto")
+    override fun autoComplete(
+        @RequestParam(name = "keyword") keyword: String,
+        @RequestParam(name = "size", defaultValue = "5") size: Int,
+        @RequestParam(name = "dong_code", required = false) addressCodes: List<String>?,
+    ): ApiResponse<List<AutoCompleteResponseDTO>> {
+        val data =
+            searchService.autoCompleteByKeyword(keyword, size).map { result ->
+                AutoCompleteResponseDTO(
+                    id = result.id,
+                    region = Region("법정동 이름", result.legalCode),
+                    roadAddresses = result.roadAddresses,
+                    category = result.category,
+                    name = result.name,
+                )
+            }.toList()
+        return ApiResponse.success(data)
+    }
+
+    @GetMapping("")
+    override fun searchTerm(
+        @RequestParam(name = "keyword") keyword: String,
+        @RequestParam(name = "size", defaultValue = "5") size: Int,
+        @RequestParam(name = "last_id", required = false) lastId: String?,
+        @RequestParam(name = "dong_code", required = false) addressCodes: List<String>?,
+        @RequestParam(name = "categories", required = false) categories: List<FoodCategory>?,
+        @RequestParam(name = "sort", required = false) sort: OrderStrategy?,
+    ): ApiResponse<CursorResponse<SearchResponseDTO>> {
+        val cursorSearch = searchService.cursorSearchByKeyword(keyword, size, lastId, addressCodes, emptyList())
+        val data =
+            cursorSearch.result.map { result ->
+                SearchResponseDTO(
+                    id = result.id,
+                    addresses = result.addresses,
+                    roadAddresses = result.roadAddresses,
+                    category = result.category,
+                    name = result.name,
+                    imageUrl = result.imageUrl,
+                    kakao =
+                        ReviewSummary(
+                            count = result.kakaoReviewCount,
+                            score = result.kakaoScore,
+                            processed = result.kakaoReview,
+                        ),
+                    naver =
+                        ReviewSummary(
+                            count = result.naverReviewCount,
+                            score = result.naverScore,
+                            processed = result.naverReview,
+                        ),
+                    region = Region("법정동 이름", result.legalCode),
+                )
+            }.toList()
 
         return ApiResponse.success(
             CursorResponse(
-                summaries,
-                hasNext,
+                content = data,
+                hasNext = cursorSearch.hasNext,
             ),
         )
     }
