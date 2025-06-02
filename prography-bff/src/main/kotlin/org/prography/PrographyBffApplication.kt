d90287f5746d727b7720a90d7d71f88cc5eddcf1
Sunghan <park.libra1011@gmail.com>
2025-06-03 00:16:41 +0900

    refactor : Search 모듈 BFF 메인 클래스에 컴포넌트 스캔 추가

--- prography-bff/src/main/kotlin/org/prography/PrographyBffApplication.kt
+++ prography-bff/src/main/kotlin/org/prography/PrographyBffApplication.kt
@@ -1,11 +1,9 @@
 package org.prography
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@ConfigurationPropertiesScan(basePackages = ["org.prography.bff", "org.prography.search"])
-@SpringBootApplication(scanBasePackages = ["org.prography.bff", "org.prography.search"])
+@SpringBootApplication(scanBasePackages = ["org.prography.search"])
 class PrographyBffApplication
 
 fun main(args: Array<String>) {
