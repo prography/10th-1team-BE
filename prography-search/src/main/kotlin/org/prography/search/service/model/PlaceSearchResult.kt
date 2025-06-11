e88191e0be0c3f60e195bdb184b8f25f9a6717f7
Sunghan <park.libra1011@gmail.com>
2025-06-11 23:24:27 +0900

    refactor : 변경된 매핑에 맞춰서 domain 변경 및 기획에 따른 정렬 방식 변경

--- prography-search/src/main/kotlin/org/prography/search/service/model/PlaceSearchResult.kt
+++ prography-search/src/main/kotlin/org/prography/search/service/model/PlaceSearchResult.kt
@@ -1,11 +1,13 @@
 package org.prography.search.service.model
 
+import org.prography.search.domain.GeoPoint
+
 /**
- * 검색
+ * 검색 결과
  */
 data class PlaceSearchResult(
     /**
-     *
+     * 아이디
      */
     val id: String,
     /**
@@ -16,15 +18,52 @@ data class PlaceSearchResult(
      * 행정 동 코드
      */
     val administrativeCode: String,
+    /**
+     * 주소
+     */
     val addresses: String,
+    /**
+     * 도로명 주소
+     */
     val roadAddresses: String,
+    /**
+     * 음식점 카테고리
+     */
     val category: String,
+    /**
+     * 상호명
+     */
     val name: String,
+    /**
+     * 대표 이미지 주소
+     */
     val imageUrl: String,
+    /**
+     * 카카오 리뷰 갯수
+     */
     val kakaoReviewCount: Long,
-    val kakaoScore: Float,
+    /**
+     * 카카오 리뷰 점수
+     */
+    val kakaoScore: Double,
+    /**
+     * 카카오 리뷰 존재 유무
+     */
     val kakaoReview: Boolean,
+    /**
+     * 네이버 리뷰 갯수
+     */
     val naverReviewCount: Long,
-    val naverScore: Float,
+    /**
+     * 네이버 리뷰 점수
+     */
+    val naverScore: Double,
+    /**
+     * 네이버 리뷰 존재 유무
+     */
     val naverReview: Boolean,
+    /**
+     * 위치 정보
+     */
+    val location: GeoPoint,
 )
--- prography-search/src/main/kotlin/org/prography/search/service/model/enumeration/SortingStrategy.kt
+++ prography-search/src/main/kotlin/org/prography/search/service/model/enumeration/SortingStrategy.kt
@@ -3,17 +3,10 @@ package org.prography.search.service.model.enumeration
 /**
  * Elasticsearch 결과 정렬 전략
  */
-enum class SortingStrategy(val sorting: String) {
-    AVERAGE_RATING_HIGH("avgRating,desc"), // 별점 높은 순 (카카오+네이버 평균)
-    AVERAGE_RATING_LOW("avgRating,asc"), // 별점 낮은 순 (카카오+네이버 평균)
-    REVIEW_COUNT_HIGH("totalReviewCount,desc"), // 리뷰 많은 순 (카카오+네이버 합산)
-    REVIEW_COUNT_LOW("totalReviewCount,asc"), // 리뷰 적은 순 (카카오+네이버 합산)
-
-    KAKAO_RATING_HIGH("kakaoScore,desc"), // 카카오 별점 높은 순
-    KAKAO_RATING_LOW("kakaoScore,asc"), // 카카오 별점 낮은 순
-    KAKAO_REVIEW_COUNT_HIGH("kakaoReviewCount,desc"), // 카카오 리뷰 많은 순
-
-    NAVER_RATING_HIGH("naverScore,desc"), // 네이버 별점 높은 순
-    NAVER_RATING_LOW("naverScore,asc"), // 네이버 별점 낮은 순
-    NAVER_REVIEW_COUNT_HIGH("naverReviewCount,desc"), // 네이버 리뷰 많은 순
+enum class SortingStrategy {
+    RELATED,
+    AVERAGE_RATING_HIGH, // 별점 높은 순 (카카오+네이버 평균)
+    AVERAGE_RATING_LOW, // 별점 낮은 순 (카카오+네이버 평균)
+    REVIEW_COUNT_HIGH, // 리뷰 많은 순 (카카오+네이버 합산)
+    REVIEW_COUNT_LOW, // 리뷰 적은 순 (카카오+네이버 합산)
 }
