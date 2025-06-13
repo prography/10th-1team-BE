1734aa0bd095485dd16290a1df80d0fcff577ed2
Sunghan <park.libra1011@gmail.com>
2025-06-14 03:10:19 +0900

    test : 커서 관련 사이즈에 맞춰서 복수적으로 호출되던 문제 수정 및 서치 정상 동작 확인 테스트

--- prography-search/src/test/kotlin/org/prography/search/service/SearchServiceTest.kt
+++ prography-search/src/test/kotlin/org/prography/search/service/SearchServiceTest.kt
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.prography.search.config.ElasticsearchConfig
 import org.prography.search.config.ElasticsearchProperty
+import org.prography.search.service.model.PlaceSearchResult
+import org.prography.search.service.model.enumeration.FilterCategory
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 class SearchServiceTest {
@@ -37,8 +39,41 @@ class SearchServiceTest {
     }
 
     @Test
+    fun `Filter Search 테스트`() {
+        val data = searchService.cursorSearchByKeyword("제육", size = 4, addressCodes = emptyList(), cursorString = "")
+        val filtered =
+            data.result.filter { r -> r.category != FilterCategory.FD01.value }
+                .toList()
+        Assertions.assertEquals(0, filtered.size)
+    }
+
+    @Test
     fun `Keyword Search 테스트`() {
-        val keyword = searchService.cursorSearchByKeyword("그릴", 5)
-        Assertions.assertNotNull(keyword)
+        val allResults = mutableListOf<PlaceSearchResult>()
+        var cursorString: String? = null
+
+        do {
+            // 첫 요청에는 cursorString == null
+            val page =
+                searchService.cursorSearchByKeyword(
+                    keyword = "제육",
+                    size = 5,
+                    cursorString = cursorString,
+                    addressCodes = emptyList(),
+                )
+
+            // 결과 누적
+            allResults += page.result
+
+            // 다음 cursor 갱신: hasNext 가 true 일 때만 cursorString 에 담고, 아니면 null 로 끊기
+            cursorString = if (page.hasNext) page.cursor else null
+        } while (cursorString != null)
+
+        Assertions.assertEquals(5, allResults.size)
+    }
+
+    @Test
+    fun `AutoComplete Search 테스트`() {
+        val keyword = searchService.autoCompleteByKeyword("용두동", 5, emptyList())
     }
 }
--- prography-search/src/test/kotlin/org/prography/search/service/model/CursorTest.kt
+++ prography-search/src/test/kotlin/org/prography/search/service/model/CursorTest.kt
@@ -10,19 +10,19 @@ class CursorTest {
         val encode = Cursor.encode("오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30")
         val cursor = Cursor.decode(encode, strategy = SortingStrategy.RELATED)
 
-        Assertions.assertNull(cursor.key)
-        Assertions.assertEquals("오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30", cursor.id)
+        Assertions.assertNull(cursor?.key)
+        Assertions.assertEquals("오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30", cursor?.id)
 
         val encode2 = Cursor.encode(4.78, "오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30")
         val decode2 = Cursor.decode(encode2, strategy = SortingStrategy.AVERAGE_RATING_LOW)
 
-        Assertions.assertEquals(4.78, decode2.key)
-        Assertions.assertEquals("오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30", decode2.id)
+        Assertions.assertEquals(4.78, decode2?.key)
+        Assertions.assertEquals("오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30", decode2?.id)
 
         val encode3 = Cursor.encode(44213, "오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30")
         val decode3 = Cursor.decode(encode3, strategy = SortingStrategy.REVIEW_COUNT_LOW)
 
-        Assertions.assertEquals(44213, decode3.key)
-        Assertions.assertEquals("오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30", decode3.id)
+        Assertions.assertEquals(44213, decode3?.key)
+        Assertions.assertEquals("오엔_카페앤레스토랑@서울_강남구_압구정로11길_37-30", decode3?.id)
     }
 }
