8fbd66e25a487616512397a7cc62f3a2370ba993
Sunghan <park.libra1011@gmail.com>
2025-06-25 23:06:05 +0900

    docs : swagger 스키마 설명 추가

--- prography-bff/src/main/kotlin/org/prography/bff/vote/controller/model/enumeration/Reason.kt
+++ prography-bff/src/main/kotlin/org/prography/bff/vote/controller/model/enumeration/Reason.kt
@@ -1,26 +1,33 @@
 package org.prography.bff.vote.controller.model.enumeration
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 /**
  * 해당 플랫폼에 투표한 이유
  */
+@Schema(description = "플랫폼에 투표한 이유")
 enum class Reason {
     /**
      * 리뷰가 많아요
      */
+    @Schema(description = "리뷰가 많아요")
     MANY_REVIEWS,
 
     /**
      * 디테일한 설명이 많아요
      */
+    @Schema(description = "디테일한 설명이 많아요")
     DETAILED,
 
     /**
      * 리뷰가 솔직해요
      */
+    @Schema(description = "리뷰가 솔직해요")
     HONEST,
 
     /**
      *설명이 정확해요
      */
+    @Schema(description = "설명이 정확해요")
     ACCURATE,
 }
