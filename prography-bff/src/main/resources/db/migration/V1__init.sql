9a4c22a99b18e44afb23d080fc9dd17a234f5c18
seogwoojin <server@uos.ac.kr>
2025-06-28 16:00:33 +0900

    feat: 데이터베이스 형상 관리 라이브러리 추가

---
+++ prography-bff/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,58 @@
+CREATE TABLE users (
+                       id UUID PRIMARY KEY,
+                       provider VARCHAR(50) NOT NULL,
+                       provider_id VARCHAR(255) NOT NULL,
+                       nickname VARCHAR(100) NOT NULL,
+                       level INTEGER NOT NULL DEFAULT 0,
+                       status BOOLEAN NOT NULL DEFAULT FALSE,
+                       created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+                       updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE TABLE province (
+                          province_code VARCHAR PRIMARY KEY,
+                          province_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE city (
+                      city_code VARCHAR PRIMARY KEY,
+                      city_name VARCHAR(50) NOT NULL,
+                      is_scraped BOOLEAN NOT NULL DEFAULT FALSE,
+                      province_code VARCHAR NOT NULL,
+                      CONSTRAINT fk_city_province
+                          FOREIGN KEY (province_code)
+                              REFERENCES province(province_code)
+                              ON DELETE CASCADE
+);
+
+CREATE TABLE dong (
+                      code VARCHAR(10) PRIMARY KEY,
+                      name VARCHAR NOT NULL,
+                      city_code VARCHAR NOT NULL,
+                      CONSTRAINT fk_dong_city
+                          FOREIGN KEY (city_code)
+                              REFERENCES city(city_code)
+                              ON DELETE CASCADE
+);
+
+-- VOTE 테이블 생성
+CREATE TABLE vote (
+                      place_id VARCHAR NOT NULL,
+                      platform_name TEXT NOT NULL,
+                      total BIGINT NOT NULL,
+                      many_reviews BIGINT NOT NULL,
+                      detailed BIGINT NOT NULL,
+                      honest BIGINT NOT NULL,
+                      accurate BIGINT NOT NULL,
+                      PRIMARY KEY (place_id, platform_name)
+);
+
+-- VOTE_HISTORY 테이블 생성
+CREATE TABLE vote_history (
+                              id BIGSERIAL PRIMARY KEY,
+                              user_id UUID NOT NULL,
+                              place_id VARCHAR NOT NULL,
+                              category TEXT NOT NULL,
+                              platform TEXT NOT NULL,
+                              voted_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
