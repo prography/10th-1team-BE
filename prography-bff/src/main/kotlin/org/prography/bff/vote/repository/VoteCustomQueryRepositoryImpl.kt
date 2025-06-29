3ae13baa5344d15f441898dba17a634e44ef7d5d
Sunghan <park.libra1011@gmail.com>
2025-06-29 23:29:24 +0900

    feat : 특정 기간에 투표된 이력을 조회하는 메소드 추가

--- prography-bff/src/main/kotlin/org/prography/bff/vote/repository/VoteCustomQueryRepositoryImpl.kt
+++ prography-bff/src/main/kotlin/org/prography/bff/vote/repository/VoteCustomQueryRepositoryImpl.kt
@@ -2,10 +2,13 @@ package org.prography.bff.vote.repository
 
 import org.prography.bff.vote.repository.custom.VoteCustomQueryRepository
 import org.prography.bff.vote.repository.model.VoteEntity
+import org.prography.bff.vote.repository.model.VoteHistoryEntity
 import org.prography.bff.vote.repository.model.VoteId
 import org.prography.bff.vote.repository.model.enumeration.VotePlatform
 import org.springframework.stereotype.Repository
-import java.util.*
+import java.time.LocalDateTime
+import java.util.Optional
+import java.util.UUID
 
 @Repository
 class VoteCustomQueryRepositoryImpl(
@@ -37,6 +40,29 @@ class VoteCustomQueryRepositoryImpl(
         userId: UUID,
         placeId: String,
     ): Boolean {
-        return historyRepository.existsByUserIdAndPlaceId(userId, placeId)
+        return historyRepository.findAll(limit = 1) {
+            select(intLiteral(1))
+                .from(entity(VoteHistoryEntity::class))
+                .where(
+                    path(VoteHistoryEntity::userId).eq(userId).and(
+                        path(VoteHistoryEntity::placeId).eq(placeId),
+                    ),
+                )
+        }.isNotEmpty()
+    }
+
+    override fun findHistoriesByVotedDateBetween(
+        userId: UUID,
+        from: LocalDateTime,
+        to: LocalDateTime,
+    ): List<VoteHistoryEntity> {
+        return historyRepository.findAll {
+            select(entity(VoteHistoryEntity::class))
+                .from(entity(VoteHistoryEntity::class))
+                .where(
+                    path(VoteHistoryEntity::votedDate).between(from, to)
+                        .and(path(VoteHistoryEntity::userId).equal(userId)),
+                )
+        }.filterNotNull()
     }
 }
--- prography-bff/src/main/kotlin/org/prography/bff/vote/repository/VoteHistoryEntityRepository.kt
+++ prography-bff/src/main/kotlin/org/prography/bff/vote/repository/VoteHistoryEntityRepository.kt
@@ -3,16 +3,8 @@ package org.prography.bff.vote.repository
 import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import org.prography.bff.vote.repository.model.VoteHistoryEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 /**
  * VoteHistoryEntity 관련 Spring Data JPA + Kotlin JDSL 인터페이스
  */
-interface VoteHistoryEntityRepository :
-    JpaRepository<VoteHistoryEntity, Long>,
-    KotlinJdslJpqlExecutor {
-    fun existsByUserIdAndPlaceId(
-        userId: UUID,
-        placeId: String,
-    ): Boolean
-}
+interface VoteHistoryEntityRepository : JpaRepository<VoteHistoryEntity, Long>, KotlinJdslJpqlExecutor
