20b0f01120363843d17e61298af591edc5f9c527
seogwoojin <server@uos.ac.kr>
2025-07-04 00:10:09 +0900

    fix: 유저 생성 시 createdAt 필드 제거

--- prography-bff/src/main/kotlin/org/prography/bff/user/domain/service/model/UserInfoDto.kt
+++ prography-bff/src/main/kotlin/org/prography/bff/user/domain/service/model/UserInfoDto.kt
@@ -2,7 +2,6 @@ package org.prography.bff.user.domain.service.model
 
 import org.prography.bff.user.domain.entity.Provider
 import org.prography.bff.user.domain.entity.User
-import java.time.LocalDateTime
 import java.util.*
 
 data class UserInfoDto(
@@ -10,7 +9,6 @@ data class UserInfoDto(
     val provider: Provider,
     val nickname: String,
     val level: Int,
-    val createdAt: LocalDateTime,
     val isNewUser: Boolean,
 ) {
     companion object {
@@ -23,7 +21,6 @@ data class UserInfoDto(
                 provider = user.provider,
                 nickname = user.nickname,
                 level = user.level,
-                createdAt = user.createdAt,
                 isNewUser = isNewUser,
             )
         }
