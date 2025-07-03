4b2c6f90a194ddab37551f26b648b02a9a638238
seogwoojin <server@uos.ac.kr>
2025-07-04 00:22:48 +0900

    fix: 새 유저 생성 시 createdAt 참조 제거

--- prography-bff/src/main/kotlin/org/prography/bff/user/domain/service/model/UserInfoDto.kt
+++ prography-bff/src/main/kotlin/org/prography/bff/user/domain/service/model/UserInfoDto.kt
@@ -2,6 +2,7 @@ package org.prography.bff.user.domain.service.model
 
 import org.prography.bff.user.domain.entity.Provider
 import org.prography.bff.user.domain.entity.User
+import java.time.LocalDateTime
 import java.util.*
 
 data class UserInfoDto(
@@ -9,6 +10,7 @@ data class UserInfoDto(
     val provider: Provider,
     val nickname: String,
     val level: Int,
+    val createdAt: LocalDateTime,
     val isNewUser: Boolean,
 ) {
     companion object {
@@ -21,7 +23,19 @@ data class UserInfoDto(
                 provider = user.provider,
                 nickname = user.nickname,
                 level = user.level,
+                createdAt = user.createdAt,
                 isNewUser = isNewUser,
+            )
+        }
+
+        fun fromNewUser(user: User): UserInfoDto {
+            return UserInfoDto(
+                userId = user.id,
+                provider = user.provider,
+                nickname = user.nickname,
+                level = user.level,
+                createdAt = LocalDateTime.now(),
+                isNewUser = true,
             )
         }
     }
